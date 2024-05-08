110276cdb27b7647faf06a4e71fb7e8844bfbd77
smeb y <48400087+a3165458@users.noreply.github.com>
2024-05-08 14:43:56 +0800

    Update 0g.sh

--- 0g.sh
+++ 0g.sh
@@ -91,7 +91,9 @@ function install_node() {
     0gchaind validate-genesis
     
     # 配置节点
-    SEEDS="c4d619f6088cb0b24b4ab43a0510bf9251ab5d7f@54.241.167.190:26656,44d11d4ba92a01b520923f51632d2450984d5886@54.176.175.48:26656,f2693dd86766b5bf8fd6ab87e2e970d564d20aff@54.193.250.204:26656,f878d40c538c8c23653a5b70f615f8dccec6fb9f@54.215.187.94:26656" && \
+    SEEDS="c4d619f6088cb0b24b4ab43a0510bf9251ab5d7f@54.241.167.190:26656,44d11d4ba92a01b520923f51632d2450984d5886@54.176.175.48:26656,f2693dd86766b5bf8fd6ab87e2e970d564d20aff@54.193.250.204:26656,f878d40c538c8c23653a5b70f615f8dccec6fb9f@54.215.187.94:26656"
+    PEERS="75a398f9e3a7d24c6b3ba4ab71bf30cd59faee5c@95.216.42.217:26656"
+    sed -i "s/peers = \"\"/peers = \"$PEERS\"/" $HOME/.0gchain/config/config.toml
     sed -i "s/seeds = \"\"/seeds = \"$SEEDS\"/" $HOME/.0gchain/config/config.toml
 
 
