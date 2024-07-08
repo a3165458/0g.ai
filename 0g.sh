e2b8c1b7feb715ad20c3c32785e49181f836b981
smeb y <48400087+a3165458@users.noreply.github.com>
2024-07-08 18:01:25 +0800

    Update 0g.sh

--- 0g.sh
+++ 0g.sh
@@ -223,8 +223,10 @@ cd run
 
 
 read -p "请输入你想导入的EVM钱包私钥，不要有0x: " minerkey
+read -p "请输入设备IP地址: " new_address
 
 sed -i "s/miner_key = \"\"/miner_key = \"$minerkey\"/" config.toml
+sed -i "s|^# *network_enr_address = \".*\"|network_enr_address = \"$new_address\"|" config_file.toml
 sed -i 's/log_sync_start_block_number = [0-9]*/log_sync_start_block_number = 0/' config.toml
 
 screen -dmS zgs_node_session ../target/release/zgs_node --config config.toml
